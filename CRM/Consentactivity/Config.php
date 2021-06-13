a0c6234985fc2c6f2b53921d17c5501c3fc5c368
akosgarai <szlem01@gmail.com>
2021-06-13 17:39:10 +0200

    Config class implementation

---
+++ CRM/Consentactivity/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+class CRM_Consentactivity_Config extends CRM_RcBase_Config
+{
+    /**
+     * Provides a default configuration object.
+     * The activity-type defaults to 0, as it is an invalid activity
+     * type id.
+     *
+     * @return array the default configuration object.
+     */
+    public function defaultConfiguration(): array
+    {
+        return [
+            'activity-type-id' => 0,
+        ];
+    }
+}
