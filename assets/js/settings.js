f294329a31c4764e02797604df09cdf248744a79
akosgarai <szlem01@gmail.com>
2021-10-28 16:57:07 +0200

    Don't copy error messages

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -11,6 +11,7 @@ function mapParameterMappingHandler(event) {
     newRow.querySelectorAll('select[name*="map_custom_field_id_"]').forEach(function(element) {element.value = '0'; element.name = 'map_custom_field_id_'+numberOfItems; element.id = 'map_custom_field_id_'+numberOfItems;});
     newRow.querySelectorAll('select[name*="map_consent_field_id_"]').forEach(function(element) {element.value = '0'; element.name = 'map_consent_field_id_'+numberOfItems; element.id = 'map_consent_field_id_'+numberOfItems;});
     newRow.querySelectorAll('select[name*="map_group_id_"]').forEach(function(element) {element.value = '0'; element.name = 'map_group_id_'+numberOfItems; element.id = 'map_group_id_'+numberOfItems;});
+    newRow.querySelectorAll('.crm-error').forEach(function(element) {element.remove();});
     let buttonRow = document.querySelector('#new-parameter-mapping-row');
     buttonRow.parentNode.insertBefore(newRow, buttonRow);
 };
