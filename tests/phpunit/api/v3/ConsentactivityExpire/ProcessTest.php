0a4ad003d22bc0f8962e863e7452f48df637ddba
akosgarai <szlem01@gmail.com>
2021-10-31 19:29:21 +0100

    Extend the expiratoin process test

--- tests/phpunit/api/v3/ConsentactivityExpire/ProcessTest.php
+++ tests/phpunit/api/v3/ConsentactivityExpire/ProcessTest.php
@@ -4,6 +4,11 @@ use CRM_Consentactivity_ExtensionUtil as E;
 use Civi\Api4\Contact;
 use Civi\Api4\Activity;
 use Civi\Api4\EntityTag;
+use Civi\Api4\Phone;
+use Civi\Api4\Website;
+use Civi\Api4\IM;
+use Civi\Api4\Email;
+use Civi\Api4\Address;
 
 /**
  * ConsentactivityExpire.Process API Test Case
@@ -55,8 +60,7 @@ class api_v3_ConsentactivityExpire_ProcessTest extends CRM_Consentactivity_Headl
     /**
      * Test Process action with setting the search.
      * Actions and tag will be set for 1 contact, so that
-     * it has to return error as that part of the feature
-     * is not implemented yet.
+     * it has to return it as the handled one.
      */
     public function testApiProcessWithContact()
     {
@@ -93,8 +97,40 @@ class api_v3_ConsentactivityExpire_ProcessTest extends CRM_Consentactivity_Headl
                 ->addValue('entity_id', $contact['id'])
                 ->addValue('tag_id', $config['tag-id'])
                 ->execute();
-        self::expectException(CiviCRM_API3_Exception::class);
-        self::expectExceptionMessage('This feature is unimplemented.');
-        civicrm_api3('ConsentactivityExpire', 'process', []);
+        $result = civicrm_api3('ConsentactivityExpire', 'process', []);
+        self::assertSame(1, $result['values']['handled'], 'Invalid number of handled: '.$result['values']['handled']);
+        self::assertCount(0, $result['values']['errors']);
+        $updatedContact = Contact::get(false)
+            ->addWhere('id', '=', $contact['id'])
+            ->setLimit(1)
+            ->addSelect('do_not_email', 'do_not_phone', 'do_not_mail', 'do_not_sms', 'do_not_trade', 'is_opt_out',
+                'first_name', 'last_name', 'middle_name', 'display_name', 'email_greeting_display', 'postal_greeting_display',
+                'addressee_display', 'nick_name', 'sort_name', 'external_identifier', 'image_url', 'api_key', 'birth_date',
+                'deceased_date', 'employer_id', 'job_title', 'gender_id')
+            ->execute()
+            ->first();
+        $contactFieldsThatNeedsToBeDeleted = [
+            'first_name', 'last_name', 'middle_name', 'display_name',
+            'nick_name', 'sort_name',
+            'external_identifier', 'api_key', 'birth_date',
+            'deceased_date', 'employer_id', 'job_title', 'gender_id',
+        ];
+        foreach ($contactFieldsThatNeedsToBeDeleted as $field) {
+            self::assertEmpty($updatedContact[$field], 'The '.$field.' field should be empty, but it is '.$updatedContact[$field]);
+        }
+        $privacyFieldsThatNeedsToBeSet = [
+            'do_not_email', 'do_not_phone', 'do_not_mail',
+            'do_not_sms', 'do_not_trade', 'is_opt_out',
+        ];
+        foreach ($privacyFieldsThatNeedsToBeSet as $field) {
+            self::assertTrue($updatedContact[$field], 'The consent field '.$field.' should be set.');
+        }
+        foreach (CRM_Consentactivity_Service::CONTACT_DATA_ENTITIES as $entity) {
+            $numberOfEntities = $entity::get(false)
+                ->addWhere('contact_id', '=', $contact['id'])
+                ->selectRowCount()
+                ->execute();
+            self::assertCount(0, $numberOfEntities, 'Invalid number for the '.$entity.' entity.');
+        }
     }
 }
