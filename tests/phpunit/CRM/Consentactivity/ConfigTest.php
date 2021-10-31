f27d64da7a02c98def7efad25c275b4b80ed7fcb
akosgarai <szlem01@gmail.com>
2021-10-31 19:33:04 +0100

    Testcase for the termination group

--- tests/phpunit/CRM/Consentactivity/ConfigTest.php
+++ tests/phpunit/CRM/Consentactivity/ConfigTest.php
@@ -25,6 +25,8 @@ class CRM_Consentactivity_ConfigTest extends CRM_Consentactivity_HeadlessBase
         self::assertSame(CRM_Consentactivity_Config::DEFAULT_TAG_SEARCH_ID, $cfg['tagging-search-id'], 'Invalid tagging-search-id initial value.');
         self::assertTrue(array_key_exists('tag-id', $cfg), 'tag-id key is missing from the config.');
         self::assertSame(CRM_Consentactivity_Config::DEFAULT_TAG_ID, $cfg['tag-id'], 'Invalid tag-id initial value.');
+        self::assertTrue(array_key_exists('termination-group-id', $cfg), 'termination-group-id key is missing from the config.');
+        self::assertSame(CRM_Consentactivity_Config::DEFAULT_TERMINATION_GROUP_ID, $cfg['termination-group-id'], 'Invalid termination-group-id initial value.');
         self::assertTrue(array_key_exists('consent-expiration-years', $cfg), 'consent-expiration-years key is missing from the config.');
         self::assertSame(CRM_Consentactivity_Config::DEFAULT_CONSENT_EXPIRATION_YEAR, $cfg['consent-expiration-years'], 'Invalid consent-expiration-years initial value.');
         self::assertTrue(array_key_exists('consent-expiration-tagging-days', $cfg), 'consent-expiration-tagging-days key is missing from the config.');
