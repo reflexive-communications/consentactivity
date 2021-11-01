b8d8278713c43c92248ccbc5c33825a6f7c5b92f
akosgarai <szlem01@gmail.com>
2021-11-01 15:08:16 +0100

    Consent renewal page implementation

--- CRM/Consentactivity/Page/ConsentRenew.php
+++ CRM/Consentactivity/Page/ConsentRenew.php
@@ -5,11 +5,23 @@ class CRM_Consentactivity_Page_ConsentRenew extends CRM_Core_Page
 {
     public function run()
     {
-        // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-        CRM_Utils_System::setTitle(E::ts('ConsentRenew'));
-
-        // Example: Assign a variable for use in a template
-        $this->assign('currentTime', date('Y-m-d H:i:s'));
+        // URL validation.
+        $jobId = CRM_Utils_Request::retrieve('jid', 'Int');
+        $queueId = CRM_Utils_Request::retrieve('qid', 'Int');
+        $hash = CRM_Utils_Request::retrieve('h', 'String');
+        if (!$jobId || !$queueId || !$hash) {
+            throw new CRM_Core_Exception(E::ts('Missing input parameters'));
+        }
+        // verify that the three numbers above match
+        $q = CRM_Mailing_Event_BAO_Queue::verify($jobId, $queueId, $hash);
+        if (!$q) {
+            throw new CRM_Core_Exception(E::ts('There was an error in your request'));
+        }
+        $activity = CRM_Consentactivity_Service::createConsentActivityToContact($q->contact_id);
+        if (!count($activity)) {
+            throw new CRM_Core_Exception(E::ts('Failed to renew consent.'));
+        }
+        CRM_Utils_System::setTitle(E::ts(''));
 
         parent::run();
     }
